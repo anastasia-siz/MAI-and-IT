a2eb9a9bacb3672020754bcfbf7c025b01ae84b8
anastasia-siz <dashazzz80ddd@gmail.com>
2025-12-12 20:07:43 +0300

    Add files via upload

---
+++ перевод времени.py
@@ -0,0 +1,7 @@
+import numpy as np
+from astropy.time import Time
+times = ['2025-12-12T00:00:00.123456789', '2007-02-21T00:00:00']
+t = Time(times, format='isot', scale='utc')
+print('UTC :', t)
+print('TT :', t.tt)
+print('TAI :', t.tai)
